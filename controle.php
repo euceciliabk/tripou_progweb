ea8193521ab6f1d8e3cdd03adbbb4ba52f4f17b5
euceciliabk <51093324+euceciliabk@users.noreply.github.com>
2021-02-28 16:15:38 -0300

    Add files via upload

---
+++ controle.php
@@ -0,0 +1,17 @@
+<?php
+require_once "usuario/controle.php";
+require_once "mensagem/controle.php";
+require_once "persistencia/controle.php";
+
+function criaControleUsuario() {
+    $persistencia = criaPersistenciaUsuario();
+    $controleUsuario = new ControleUsuario ($persistencia);
+    return $controleUsuario;
+}
+
+function criaControleMensagem() {
+    $persistencia = criaPersistenciaMensagem();
+    $controleUsuario = new ControleMensagem ($persistencia);
+    return $controleMensagem;
+}
+?>
