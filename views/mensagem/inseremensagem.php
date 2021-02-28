317d18e47cdaddd8fe6ca2cc9a142744450d2690
euceciliabk <51093324+euceciliabk@users.noreply.github.com>
2021-02-28 16:12:51 -0300

    Add files via upload

---
+++ views/mensagem/inseremensagem.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once "controle.php";
+
+$controleUsuario = criaControleUsuario();
+
+$login = $controleUsuario->getLogin();
+
+if (!empty($login)) {
+	require_once 'views/mensagem/inseremensagemform.html';
+	
+}
+	
+?>
---
+++ views/mensagem/listamensagens.php
@@ -0,0 +1,13 @@
+<?php
+	require_once "controle.php";
+	$controleMensagem = criaControleMensagem();
+	$mensagens = $controleMensagem -> getMensagens();
+	
+	foreach($mensagens as $mensagem) {
+		echo "<br><div>";
+		echo $mensagem['quem'] . "(". $mensagem['quando'] . ")<br>";
+		echo $mensagem['texto'];
+		require 'views/mensagem/removemsgform.php';
+		echo "</div>";
+	}
+?>
