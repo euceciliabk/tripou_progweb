6418a228731d2d22342bbf72ab6e93a02781609d
euceciliabk <51093324+euceciliabk@users.noreply.github.com>
2021-03-01 19:28:51 -0300

    Delete inseremensagemaction.php

--- views/mensagem/inseremensagemaction.php
+++
@@ -1,9 +0,0 @@
-<?php
-chdir("../../");
-require_once "controle.php";
-$controleMensagem = new criaControleMensagem();
-$controleMensagem->insereMensagem($_POST['texto']);
-
-header("Location: /tripou/");
-
-?>
