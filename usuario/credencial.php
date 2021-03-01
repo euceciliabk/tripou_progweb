9400d37be42cc272f780dbff7aa9ca480407146a
euceciliabk <51093324+euceciliabk@users.noreply.github.com>
2021-03-01 19:28:14 -0300

    Add files via upload

---
+++ usuario/credencial.php
@@ -0,0 +1,28 @@
+<?php
+
+interface PersisteCredencial {
+	function insereUsuario($login, $senha);
+	function getSenha($login);
+}
+
+class Credencial {
+	private $persistencia;
+	
+	function __construct(PersisteCredencial $persistencia){
+		$this->persistencia = $persistencia;
+	}
+	
+	function confereLoginSenha($login, $senha) {
+		$senhabd = $this->persistencia->getSenha($login);
+		if ($senhabd != NULL && $senhabd == $senha){
+			return TRUE;
+		}
+		return FALSE;
+	}
+
+	function insereLoginSenha($login, $senha) {
+		$this->persistencia->insereUsuario($login, $senha);
+	}
+}
+
+?>
