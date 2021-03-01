d0cfbf13576aea40fd00af8dc6b87fe0d872d39f
euceciliabk <51093324+euceciliabk@users.noreply.github.com>
2021-03-01 19:31:32 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title> Tripou </title>
+</head>
+
+<body>
+
+	<center>
+	<h1><img width="200px" src="imagens/logo.jpeg"  /></h1>
+	<?php require_once 'views/usuario/usuariologado.php'; ?>
+	<?php require_once 'views/mensagem/inseremensagem.php'; ?>
+	<br>
+	<?php require_once 'views/mensagem/listamensagens.php'; ?>
+	</center>
+	
+</body>
+
+</html>
+
